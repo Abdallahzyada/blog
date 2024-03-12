ba9928083b3fb8ce545979de51e55d3f14a301fc
unknown <abdallahzyada25@gmail.com>
2024-03-12 14:20:29 +0200

    retrieving posts by similar tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,8 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 # Create your views here.
 
 
@@ -40,12 +42,16 @@ def post_detail(request, year, month, day, post):
                              status = Post.Status.PUBLISHED)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tag.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tag__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tag')).order_by('-same_tags', '-publish')[:4]
     
     return render(request,
                   'blog/post/detail.html',
                   {'post' : post,
                    'comments':comments,
-                   'form':form})
+                   'form':form,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
 
