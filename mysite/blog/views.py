1bbe8f2c4a42d5c7e30b457b44594603d6efb7ec
unknown <abdallahzyada25@gmail.com>
2024-03-17 16:40:09 +0200

    adding search engin

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 # Create your views here.
 
@@ -97,3 +98,22 @@ def post_comment(request, post_id):
                       {'form' : form,
                        'post' : post,
                        'comment' : comment})
+    
+def search_post(request):
+    
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title', 'body'),
+            ).filter(search=query)
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form' : form,
+                   'query' : query,
+                   'results' : results})
